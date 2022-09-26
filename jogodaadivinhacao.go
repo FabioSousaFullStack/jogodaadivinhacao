be4acefda32cc7468685afaffa370b7c60911e94
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-26 10:32:20 -0300

    jogo da adivinhação

---
+++ jogodaadivinhacao.go
@@ -0,0 +1,28 @@
+package main 
+
+import(
+	"fmt"
+	"math/rand"
+)
+
+func main(){
+	numeroRandomico := rand.Intn(100)
+
+	var numeroDigitado int
+
+	for numeroDigitado != numeroRandomico{
+
+		fmt.Print("Digite um valor entre 0 e 100: ")
+		fmt.Scan(&numeroDigitado)
+
+		if numeroDigitado > numeroRandomico{
+			fmt.Println("Você errou! O número digitado é maior.")
+		}else if  numeroDigitado < numeroRandomico {
+			fmt.Println("Você errou! O número digitado é menor.")
+		}else{
+			fmt.Println("Parabéns você acertou, fim de jogo.")
+		}
+	}
+
+	
+}
