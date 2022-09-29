6973282e3bb11f6be5b241671690cea312f47dfa
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-28 22:10:08 -0300

    inserindo switch ao game

--- jogodaadivinhacao.go
+++ jogodaadivinhacao.go
@@ -6,13 +6,36 @@ import(
 )
 
 func main(){
-	numeroRandomico := rand.Intn(100)
+	var nivelEscolhido int
 
+	fmt.Println("1. Nível Fácil, números entre 0 e 100")
+	fmt.Println("2. Nivel médio, números entre 0 e 1000")
+	fmt.Println("3. Nível difícil, números entre 0 e 10000")
+	fmt.Print("Digite o nivel: ")
+	fmt.Scan(&nivelEscolhido)
+
+	
+	var nivel int
+
+	switch nivel{
+		case 1:
+			nivelEscolhido = 300
+	
+		case 2:
+			nivelEscolhido = 1000
+		
+		case 3:
+			nivelEscolhido=10000
+
+	}
+	
+	numeroRandomico := rand.Intn(nivelEscolhido)
+	
 	var numeroDigitado int
-
+	
 	for numeroDigitado != numeroRandomico{
-
-		fmt.Print("Digite um valor entre 0 e 100: ")
+		
+		fmt.Print("Digite um número: ")
 		fmt.Scan(&numeroDigitado)
 
 		if numeroDigitado > numeroRandomico{
@@ -23,6 +46,5 @@ func main(){
 			fmt.Println("Parabéns você acertou, fim de jogo.")
 		}
 	}
-
 	
 }
